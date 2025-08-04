a705161ea6a03ca5069cd96becdc519836f3b59a
2300030377 <163734503+2300030377@users.noreply.github.com>
2025-08-04 19:41:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
 export default defineConfig({
-  base: '/ecomJenkinsPractice/',
   plugins: [react()],
-});
+  base: '/ecomJenkinPractice',
+})
